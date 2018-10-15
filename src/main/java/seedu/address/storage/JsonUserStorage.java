4b68d4fbb859904e33309e353534ed4491b25c7a
Unknown <geraldcdx5@gmail.com>
2018-10-15 22:14:45 +0800

    changing code

--- src/main/java/seedu/address/storage/JsonUserStorage.java
+++ src/main/java/seedu/address/storage/JsonUserStorage.java
@@ -1,39 +1,56 @@
 package seedu.address.storage;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Optional;
 
-import seedu.address.commons.exceptions.DataConversionException;
-import seedu.address.commons.util.JsonUtil;
-import seedu.address.model.user.User;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
- * A class to access Users stored in the hard disk as a JSON file.
+ * A class to access UserAccount stored in the hard disk as a JSON file
  */
-public class JsonUserStorage implements UserStorage {
+public class JsonUserStorage {
 
+    private Path folderPath;
     private Path filePath;
+    private String folderPathString;
 
-    public JsonUserStorage(Path filePath) {
+    public JsonUserStorage(Path folderPath, Path filePath) throws IOException {
         this.filePath = filePath;
-    }
+        this.folderPath = folderPath;
+        folderPathString = "./" + folderPath.toString();
 
-    @Override
-    public Path getUserFilePath() {
-        return filePath;
-    }
-
-    @Override
-    public Optional<User> readUser() throws DataConversionException {
-        return readUser(filePath);
+        if (Files.notExists(filePath)) {
+            createUserFile();
+        }
     }
 
     /**
-     * Similar to {@link #readUser()}
-     * @param userFilePath location of the data. Cannot be null.
-     * @throws DataConversionException if the file format is not as expected.
+     * Returns the user account JSON as a hash map JSON object.
      */
-    private Optional<User> readUser(Path userFilePath) throws  DataConversionException {
-        return JsonUtil.readJsonFile(userFilePath, User.class);
+    public JSONObject getUserAccounts() throws IOException, ParseException {
+        JSONParser parser = new JSONParser();
+        Object object = parser.parse(new FileReader(folderPathString));
+        return (JSONObject) object;
+    }
+
+    /**
+     * Creates a user account JSON file.
+     */
+    @SuppressWarnings("unchecked")
+    private void createUserFile() throws IOException {
+        Files.createDirectory(folderPath);
+        Files.createFile(filePath);
+
+        JSONObject object = new JSONObject();
+        object.put("admin", "root");
+
+        FileWriter file = new FileWriter(folderPathString);
+        file.write(object.toJSONString());
+        file.flush();
     }
 }
